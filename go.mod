ccc99e08cec7103dd15a8fca007d237ed2dd2455
150110059 <mustafaakgilli@gmail.com>
2022-08-20 00:27:20 +0300

    adsada

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mustafaakin/docker-kubernetes-egitimi
+module github.com/150110059/goservice
 
 go 1.14
--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Merhaba Devops")
+		fmt.Fprintf(w, "Merhaba Devops! Author Mustafa AKGİLLİ")
 	})
 	http.ListenAndServe(":8080", nil)
 }
