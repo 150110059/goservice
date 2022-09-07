b4262d23c64d4862f4942c81fc971e73bcd3e7d7
150110059 <mustafaakgilli@gmail.com>
2022-09-07 12:21:29 +0300

    mustafa

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Ziraat Team from @Mustafa AKGİLLİ222")
+		fmt.Fprintf(w, "Hello Ziraat Team from @Mustafa AKGİLLİ3456")
 	})
 	http.ListenAndServe(":8080", nil)
 }
